5861c56a593b8cadd3f539b7bf6e127ac42ee506
Mithat Ercan <mithat.ercan@hotmail.com>
2022-05-24 11:25:13 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
 	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		ignoreBuildErrors: true
-	},eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+	  // !! WARN !!
+	  // Dangerously allow production builds to successfully complete even if
+	  // your project has type errors.
+	  // !! WARN !!
+	  ignoreBuildErrors: true
+	},
+	eslint: {
+    	  // Warning: This allows production builds to successfully complete even if
+         // your project has ESLint errors.
+        ignoreDuringBuilds: true,
   },
 }
